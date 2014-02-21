dd46ad9c0b4cecfa124a1275e3d0ae9300caa9de
Peat Bakke <peat@peat.org>
2014-02-21 11:47:29 -0800

    Scala modes

--- modes.el
+++ modes.el
@@ -1,6 +1,10 @@
-;; web-mode file types
+;; web
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
 
-;; ruby mode files
+;; ruby
 (add-to-list 'auto-mode-alist '("Vagrantfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+
+;; scala
+(add-to-list 'auto-mode-alist '("\\.scala\\'" . scala-mode))
+(add-to-list 'auto-mode-alist '("\\.sbt\\'" . scala-mode))
