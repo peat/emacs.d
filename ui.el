a0571e7492768d580e77b6780c50383a0bd64e01
Peat Bakke <peat@peat.org>
2014-10-21 09:35:04 -0700

    Removing powerline, adding col/line numbers

--- ui.el
+++ ui.el
@@ -10,17 +10,24 @@
 (add-to-list 'default-frame-alist (cons 'width 150))
 (add-to-list 'default-frame-alist (cons 'height 40))
 
-;; font: Inconsolata, 13pt
+;; font: Inconsolata, 16pt
 (set-face-attribute 'default nil :family "Inconsolata")
 (set-face-attribute 'default nil :height 160)
+(setq-default line-spacing 2)
 
 ;; adjust mouse scrolling behavior to be sane
 (setq mouse-wheel-scroll-amount '(1))
 (setq mouse-wheel-progressive-speed nil)
 
+;; line and column numbers, yo
+(column-number-mode 1)
+
 ;; powerline
-(add-to-list 'load-path "~/.emacs.d/vendor")
-(require 'powerline)
+; (add-to-list 'load-path "~/.emacs.d/vendor/emacs-powerline")
+; (setq powerline-color1 "grey22")
+; (setq powerline-color2 "grey40")
+; (require 'powerline)
+
 
 ;; open default org file
 (find-file "~/scratch.org")
